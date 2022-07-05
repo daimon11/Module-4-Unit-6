95b0905492109fd190f94c14da63dc8c9d3547c6
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-07-05 15:15:24 +0300

    07-07-22(15-15)

--- Task02/script.js
+++ Task02/script.js
@@ -1,9 +1,12 @@
 const allCashbox = [4500, 3210, 650, 1250, 7830, 990, 13900, 370];
-let sum = 0;
+console.log(allCashbox);
 
 const cheque = (allCashbox) => {
-  sum = allCashbox.map(i=>x+=i, x=0).reverse()[0];
-  console.log(sum);
+  let sum = 0;
+  for (let i = 0; i < allCashbox.length; i++) {
+    sum += allCashbox[i];
+    console.log(sum);
   }
-
-cheque(allCashbox);
+  return sum / allCashbox.length;
+}
+console.log(cheque(allCashbox));
