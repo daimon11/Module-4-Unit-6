3605fe71a08709ae04f9df9c867308bfe5b85a34
Dmitriy Bezgodko <daimon.94@mail.ru>
2022-07-05 15:16:26 +0300

    05-07-22(15-16)

--- Task01/script.js
+++ Task01/script.js
@@ -1,9 +1,25 @@
-const allStudents = ['Иванов','Петров','Сидоров','Кузнецов','Смирнов','Попов','Соколов'];
-const faledStudents = ['Сидоров','Смирнов','Попов'];
+const allStudents = ['Иванов', 'Петров', 'Сидоров', 'Кузнецов', 'Смирнов', 'Попов', 'Соколов'];
+const faledStudents = ['Сидоров', 'Смирнов', 'Попов'];
 
-const filter = () => {
-  let difference = allStudents.filter(num => !faledStudents.includes(num));
-  return difference;
+console.log(allStudents[key]);
+
+const filter = (allStudents, faledStudents) => {
+  let result = [];
+  let a = [];
+  for (let i = 0; i < allStudents.length; i++) {
+    a[allStudents[i]] = true;
+  }
+  for (let i = 0; i < faledStudents.length; i++) {
+    if (a[faledStudents[i]]) {
+      delete a[faledStudents[i]];
+    } else {
+      a[faledStudents[i]] = true;
+    };
+  }
+  for (let k in a) {
+    result.push(k);
+  }
+  return result;
 }
 
-console.log(filter())
+console.log(filter(allStudents, faledStudents));
